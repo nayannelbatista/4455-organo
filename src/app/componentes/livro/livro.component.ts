f633b673a684a3dd4b43256742ab710c2787007b
nayannelbatista <nayannelopes.bd@gmail.com>
2025-01-21 14:43:43 -0300

    2.1 - Trabalhando com interpolação

---
+++ src/app/componentes/livro/livro.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-livro',
+  imports: [],
+  templateUrl: './livro.component.html',
+  styleUrl: './livro.component.css'
+})
+export class LivroComponent {
+
+  livro = {
+    titulo: "As ondas",
+    autoria: "Virginia Woolf"
+  }
+}
