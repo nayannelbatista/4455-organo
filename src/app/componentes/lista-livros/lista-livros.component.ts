e303bc7b5afb1a440a90bc333e0ef6f33cc81fad
nayannelbatista <nayannelopes.bd@gmail.com>
2025-01-23 15:18:40 -0300

    4.1 - Agrupando livros por genero

--- src/app/componentes/lista-livros/lista-livros.component.ts
+++ src/app/componentes/lista-livros/lista-livros.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GeneroLiterario, Livro } from '../livro/livro';
+import { livros } from '../../mock-livros';
 
 @Component({
   selector: 'app-lista-livros',
@@ -6,6 +8,52 @@ import { Component } from '@angular/core';
   templateUrl: './lista-livros.component.html',
   styleUrl: './lista-livros.component.css'
 })
-export class ListaLivrosComponent {
+export class ListaLivrosComponent implements OnInit{
+  generos: GeneroLiterario[] = [];
+  livrosPorGenero: Map<string, Livro[]> = new Map();
+
+  ngOnInit() {
+    this.livrosPorGenero = new Map();
+
+    livros.forEach((livro) => {
+      const generoId = livro.genero.id
+      if(!this.livrosPorGenero.has(generoId)) {
+        this.livrosPorGenero.set(generoId, [])
+      }
+      this.livrosPorGenero.get(generoId)?.push(livro)
+    })
+
+    this.generos = [
+      {
+        id: 'romance',
+        value: "Romance",
+        livros: this.livrosPorGenero.get("romance") ?? []
+      },
+      { 
+        id: 'misterio', 
+        value: 'Mistério', 
+        livros: this.livrosPorGenero.get("misterio") ?? [] 
+      },
+      { 
+        id: 'fantasia', 
+        value: 'Fantasia', 
+        livros: this.livrosPorGenero.get("fantasia") ?? [] 
+      },
+      { 
+        id: 'ficcao-cientifica', 
+        value: 'Ficção Científica', 
+        livros: this.livrosPorGenero.get("ficcao-cientifica") ?? [] 
+      },
+      { 
+        id: 'tecnicos', 
+        value: 'Técnicos', 
+        livros: this.livrosPorGenero.get("tecnicos") ?? [] 
+      }
+  
+    ]
+
+    console.log(this.livrosPorGenero)
+  }
+  
 
 }
