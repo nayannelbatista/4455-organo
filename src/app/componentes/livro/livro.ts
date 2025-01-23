d94766f8e849699e847935b43c4cc9eb82da4844
nayannelbatista <nayannelopes.bd@gmail.com>
2025-01-23 14:35:36 -0300

    3.3 - criando input required

--- src/app/componentes/livro/livro.ts
+++ src/app/componentes/livro/livro.ts
@@ -3,5 +3,11 @@ export interface Livro {
   autoria: string;
   imagem: string;
   favorito: boolean;
-  genero: string;
+  genero: GeneroLiterario;
+}
+
+export interface GeneroLiterario {
+  id: string;
+  value: string;
+  livros: Livro[]
 }
